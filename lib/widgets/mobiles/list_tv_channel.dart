4b2012497efa36ae5e6627bbdeb3560b1d121e19
Bunchhoeun <bunchhoeun.chhim@gmail.com>
2024-07-30 22:16:24 +0700

    home menu

---
+++ lib/widgets/mobiles/list_tv_channel.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ListTVChannelMB extends StatefulWidget {
+
+  const ListTVChannelMB({
+    super.key,
+  });
+
+  @override
+  State<ListTVChannelMB> createState() => _ListTVChannelMBState();
+}
+
+class _ListTVChannelMBState extends State<ListTVChannelMB> {
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
